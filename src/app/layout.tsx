bcc333d29367ece7f2559cdf7ccac1ef9dacf1c3
zahid-official <zahid.official8@gmail.com>
2025-12-26 16:06:00 +0600

    Change font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,38 @@
 import { ThemeProvider } from "@/provider/theme-provider";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const suisse = localFont({
+  variable: "--font-suisse",
+  display: "swap",
+  src: [
+    {
+      path: "../assets/fonts/fonnts.com-SuisseIntl-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/fonnts.com-Suisse_Intl_Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/fonnts.com-Suisse_Intl_Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/fonnts.com-Suisse_Intl_SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/fonnts.com-Suisse_Intl_Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
 });
 
 export const metadata: Metadata = {
@@ -24,10 +46,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${suisse.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
